61cab3d1b4fbc9d3fe7e515e35e8dafc5044e25c
Dunatotatos <contact@dunatotatos.com>
2020-07-13 20:09:59 +0200

    Update code for chocolat.

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import RPi.GPIO as GPIO
 import subprocess
 import time
 import logging
-from w1thermsensor import W1ThermSensor
-from w1thermsensor.errors import ResetValueError
 
 import listener
 import constant
 
 LOG = logging.getLogger(__name__)
+
 
 class Sensor:
     def __init__(self, pin, name_get):
@@ -27,88 +26,25 @@ class Sensor:
         ])
 
     def check_run(self):
-        if self.read() and game_state[self.name_get] == False:
+        if self.read() and not game_state[self.name_get]:
             LOG.debug(game_state)
             game_state[self.name_get] = True
             LOG.debug(game_state)
             self.get_request()
 
 
-def check_run_temperature(sensor):
-    def safe_get_temperature(sensor):
-        """
-        Try hard to return the reading of the temp sensor.
-
-        This method tries to get the temperature from the sensor,
-        and catches random exceptions due to bad communication.
-        An exception is still raised if the problem of bad
-        communication is persistent.
-
-        Return
-        ------
-        int: temperature in Celcius
-
-        Raise
-        -----
-        w1thermsensor.error.ResetValueError
-            If the sensor is not reachable.
-
-        """
-        nb_exceptions = 0
-        while True:
-            try:
-                current_temperature = sensor.get_temperature()
-            except ResetValueError:
-                nb_exceptions += 1
-                LOG.warning(
-                    "Temperature sensor sent reset value %s times. Ignoring.",
-                    nb_exceptions)
-                if nb_exceptions >= 10:
-                    LOG.error("Maximum number of hardware failures reached. Stopping...")
-                    raise
-            else:
-                return current_temperature
-            time.sleep(0.1)
-
-    current_temperature = safe_get_temperature(sensor)
-    LOG.debug("Current temperature: %s.", str(current_temperature))
-
-    if current_temperature > (start_temperature + 2):
-        GPIO.output(moine, True)
-        subprocess.call([
-            "curl", "-m", "1", "-X", "GET",
-            "{}temperature".format(constant.url)
-        ])
-        time.sleep(61)
-        GPIO.output(bird, True)
-        time.sleep(50)
-        while True:
-            GPIO.output(bird, False)
-
-
 def init():
     GPIO.setmode(GPIO.BCM)
 
-    global atelier
-    global caveau
-    global serre
+    global maya
+    global console
+    global usine
     global game_state
-    global sensor_temperature
-    sensor_temperature = W1ThermSensor()
-    global start_temperature
-    start_temperature = sensor_temperature.get_temperature()
 
-    game_state = {"atelier": False, "caveau": False, "serre": False}
-    atelier = Sensor(11, "atelier")
-    caveau = Sensor(5, "caveau")
-    serre = Sensor(9, "serre")
-
-    global bird
-    bird = 21
-    global moine
-    moine = 20
-    GPIO.setup(bird, GPIO.OUT)
-    GPIO.setup(moine, GPIO.OUT)
+    game_state = {"maya": False, "console": False, "usine": False}
+    maya = Sensor(11, "maya")
+    console = Sensor(5, "console")
+    usine = Sensor(9, "usine")
 
 
 def wait_start():
@@ -130,13 +66,11 @@ def main():
         LOG.debug("Game started.")
         while (True):
             LOG.debug("Check atelier.")
-            atelier.check_run()
+            maya.check_run()
             LOG.debug("Check caveau.")
-            caveau.check_run()
+            console.check_run()
             LOG.debug("Check serre.")
-            serre.check_run()
-            LOG.debug("Check temperature.")
-            check_run_temperature(sensor_temperature)
+            usine.check_run()
     finally:
         GPIO.cleanup()
         LOG.info("Stop service.")
